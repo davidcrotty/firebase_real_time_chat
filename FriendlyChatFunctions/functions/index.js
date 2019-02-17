7ab4ebb10fa4f42983d1852f6b38fa026c04131b
David Crotty <david.crotty.work@gmail.com>
2019-02-17 20:11:50 +0000

    wip cloud function

--- FriendlyChatFunctions/functions/index.js
+++ FriendlyChatFunctions/functions/index.js
@@ -6,3 +6,24 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.emojify =
+    functions.firestore.document('/messages/{messageId}/text')
+    .onCreate((snap, context) => {
+        // Now we begin the emoji transformation
+        console.log("emojifying!");
+
+        // Get the value from the 'text' key of the message
+        const originalText = snap.data();
+        const emojifiedText = emojifyText(originalText);
+
+        // Return a JavaScript Promise to update the database node
+        snap.after.data() == {text: emojifiedText} 
+    });
+
+    function emojifyText(text) {
+        var emojifiedText = text;
+        emojifiedText = emojifiedText.replace(/\blol\b/ig, "😂");
+        emojifiedText = emojifiedText.replace(/\bcat\b/ig, "😸");
+        return emojifiedText;
+    }
